3f217435e312f4e3b7c39965278f3db7d051d049
starter-devtools <starterdevtools@gmail.com>
2024-02-05 14:15:57 -0600

    initial commit

---
+++ scripts/constants/constants.js
@@ -0,0 +1,10 @@
+export const UNMOUNT_CLASS = "unmount";
+
+export const CHANGE_CLASS = "change";
+export const SHOW_CLASS = "show";
+export const HIDE_CLASS = "hide";
+
+export const FADE_OUT_CLASS = "fade-out";
+export const FADE_IN_CLASS = "fade-in";
+
+export const FADE_OUT_TIMER = 1000;
